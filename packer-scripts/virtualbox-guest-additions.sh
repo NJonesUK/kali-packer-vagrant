dcc6c24cee955e549900d930b1d2408a4ccb918c
Nick Jones <nick@nojones.net>
2018-05-21 00:04:32 +0100

    attempts to fix ssh config bug

--- packer-scripts/virtualbox-guest-additions.sh
+++ packer-scripts/virtualbox-guest-additions.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
-sudo apt-get dist-upgrade -y
+echo "1\n" > /tmp/ssh-silence
+DEBIAN_FRONTEND=noninteractive sudo apt-get -o Dpkg::Options::="--force-confnew" dist-upgrade -yq < /tmp/ssh-silence
 sudo apt-get install -y virtualbox-guest-x11
